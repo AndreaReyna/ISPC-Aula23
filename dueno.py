536be86a8888e0b2c89be0f51123c1c7f1259b9f
Pamela Suarez <100218768+PamelSua@users.noreply.github.com>
2022-08-12 20:15:25 -0300

    tabla de tp
    trabajo integrador

---
+++ dueno.py
@@ -0,0 +1,5 @@
+<<<<<<< HEAD
+dueno =[28957346,  Juan,  Perez, 4789689,  Belgrano 101]
+=======
+dueno =[28957346,  Juan,  Perez, 4789689,  Belgrano 101]
+>>>>>>> 7db714cd77e6e1da10030d842ab2b00eebd34b97
